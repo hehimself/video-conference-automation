4f13e48810bfbe2ab7dd1ebddbbb67ed0e17f35c
Marvin Schmid <marvin_schmid@gmx.de>
2021-01-30 13:18:08 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+import os
+import pyautogui
+import time
+from time import sleep
+from datetime import datetime
+
+def open_ms_teams():
+    os.startfile()
+
+try:
+    pass
+except Exception as execption:
+    print(execption)
