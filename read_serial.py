420f1786ff5ab8ab689eda46ae422eba9efa46bf
Marvin Schmid <marvin_schmid@gmx.de>
2021-02-05 11:23:03 +0100

    Update read_serial.py

--- read_serial.py
+++ read_serial.py
@@ -5,5 +5,7 @@ try:
         x = s.readline()
         decoded_bytes = float(x[0:len(x)-2].decode("utf-8"))
         print(decoded_bytes)
+        if decoded_bytes > 21:
+            print("Hot")
 except KeyboardInterrupt:
     exit("Ende")
