5a3eae79aaf4de30ae2ceb9717fd18ba8ec35878
Marvin Schmid <marvin_schmid@gmx.de>
2021-02-05 11:29:53 +0100

    Update read_serial.py

--- read_serial.py
+++ read_serial.py
@@ -1,11 +1,17 @@
 import serial
-s = serial.Serial('COM6')
+
+try:
+    s = serial.Serial('COM6')
+except serial.serialutil.SerialException:
+    exit("Serieller Port nicht angeschlossen")
+
 try:
     while True:
-        x = s.readline()
-        decoded_bytes = float(x[0:len(x)-2].decode("utf-8"))
+        serial_message = s.readline()
+        decoded_bytes = float(serial_message[0:len(serial_message)-2].decode("utf-8"))
         print(decoded_bytes)
         if decoded_bytes > 21:
             print("Hot")
 except KeyboardInterrupt:
+    s.close()
     exit("Ende")
