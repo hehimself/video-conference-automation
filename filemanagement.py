da0be2f7bec70ff56255139fddefaac7cfde99f0
hehimself <68702979+hehimself@users.noreply.github.com>
2021-03-07 18:33:13 +0100

    Update filemanagement.py

--- filemanagement.py
+++ filemanagement.py
@@ -2,8 +2,7 @@ import glob
 import os
 import zipfile
 import subprocess
-import configparser
-import serial
+
 def teams_downloads_function(downloads_directory, directory_to_extract_to):
     downloads_directory = downloads_directory
     directory_to_extract_to = directory_to_extract_to
@@ -17,16 +16,8 @@ def teams_downloads_function(downloads_directory, directory_to_extract_to):
     latest_file = max(list_of_files, key=os.path.getctime).replace("/","\\")
     subprocess.Popen(r'explorer /select, "{latest_file}"'.format(latest_file=latest_file))
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 #     # downloads_directory = config['FILE_MANAGEMENT']['DOWNLOADS_DIRECTORY']
 #     # directory_to_extract_to = config['FILE_MANAGEMENT']['DIRECTORY_TO_EXTRACT_TO']
 #     # teams_downloads_function(downloads_directory, directory_to_extract_to)
-#     pass
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-try:
-    s = serial.Serial(config['SERIAL']['PORT'])
-except serial.serialutil.SerialException:
-    exit("Serieller Port nicht angeschlossen")
+     pass
